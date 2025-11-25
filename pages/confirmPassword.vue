35eb49a4b3e0a809391e147b68230b1e7f5850fa
Beta <jankhiawsupanida@gmail.com>
2025-11-25 13:29:57 +0700

    edit iconlogin

--- pages/confirmPassword.vue
+++ pages/confirmPassword.vue
@@ -8,15 +8,57 @@
                 <h1 class="text-4xl font-bold text-[#ffffff] text-shadow-2xl">Forgot Password</h1>
                 <h2 class="text-lg text-[#ffffff] font-bold ">Please create a new password and confirm your new password</h2>
                 
-                <div
-                    class="w-[500px] h-[50px] flex items-center bg-white/50 backdrop-blur-md border border-gray-300 rounded-4xl px-3">
-                    <!-- <img src="/img/iconUser.png" class="w-5 h-5 opacity-70" /> -->
-                    <input type="text" placeholder="Password" class="flex-1 p-3 bg-transparent rounded-4xl w-[500px] focus:outline-none text-[#ffffff] font-extrabold text-shadow-2xl" />
+                  <div
+                    class="w-[500px] h-[50px] flex items-center bg-white/55 backdrop-blur-md border border-gray-300 rounded-4xl px-3 text-[#ffffff] font-extrabold text-shadow-2xl justify-between">
+
+                    <!-- ซ้าย: ไอคอน + Password -->
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 256 256"
+                            class="text-[#737373] ml-2.5">
+                            <path fill="currentColor"
+                                d="M208 80h-32V56a48 48 0 0 0-96 0v24H48a16 16 0 0 0-16 16v112a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16m-72 78.63V184a8 8 0 0 1-16 0v-25.37a24 24 0 1 1 16 0M160 80H96V56a32 32 0 0 1 64 0Z" />
+                        </svg>
+
+                        <input type="text" placeholder="Password"
+                            class="bg-transparent focus:outline-none text-white font-bold" />
+                    </div>
+
+                    <!-- ขวาสุด: ไอคอนตา สีเทา -->
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"
+                            class="text-[#737373] mr-1.5">
+                            <circle cx="256" cy="256" r="64" fill="currentColor" />
+                            <path fill="currentColor"
+                                d="M490.84 238.6c-26.46-40.92-60.79-75.68-99.27-100.53C349 110.55 302 96 255.66 96c-42.52 0-84.33 12.15-124.27 36.11c-40.73 24.43-77.63 60.12-109.68 106.07a31.92 31.92 0 0 0-.64 35.54c26.41 41.33 60.4 76.14 98.28 100.65C162 402 207.9 416 255.66 416c46.71 0 93.81-14.43 136.2-41.72c38.46-24.77 72.72-59.66 99.08-100.92a32.2 32.2 0 0 0-.1-34.76M256 352a96 96 0 1 1 96-96a96.11 96.11 0 0 1-96 96" />
+                        </svg>
+                    </div>
+
                 </div>
                 <div
-                    class="w-[500px] h-[50px] flex items-center bg-white/50 backdrop-blur-md border border-gray-300 rounded-4xl px-3">
-                    <!-- <img src="/img/iconUser.png" class="w-5 h-5 opacity-70" /> -->
-                    <input type="text" placeholder="Confirm Password" class="flex-1 p-3 bg-transparent rounded-4xl w-[500px] focus:outline-none text-[#ffffff] font-extrabold text-shadow-2xl" />
+                    class="w-[500px] h-[50px] flex items-center bg-white/55 backdrop-blur-md border border-gray-300 rounded-4xl px-3 text-[#ffffff] font-extrabold text-shadow-2xl justify-between">
+
+                    <!-- ซ้าย: ไอคอน + Password -->
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 256 256"
+                            class="text-[#737373] ml-2.5">
+                            <path fill="currentColor"
+                                d="M208 80h-32V56a48 48 0 0 0-96 0v24H48a16 16 0 0 0-16 16v112a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16m-72 78.63V184a8 8 0 0 1-16 0v-25.37a24 24 0 1 1 16 0M160 80H96V56a32 32 0 0 1 64 0Z" />
+                        </svg>
+
+                        <input type="text" placeholder="Confirm Password"
+                            class="bg-transparent focus:outline-none text-white font-bold" />
+                    </div>
+
+                    <!-- ขวาสุด: ไอคอนตา สีเทา -->
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"
+                            class="text-[#737373] mr-1.5">
+                            <circle cx="256" cy="256" r="64" fill="currentColor" />
+                            <path fill="currentColor"
+                                d="M490.84 238.6c-26.46-40.92-60.79-75.68-99.27-100.53C349 110.55 302 96 255.66 96c-42.52 0-84.33 12.15-124.27 36.11c-40.73 24.43-77.63 60.12-109.68 106.07a31.92 31.92 0 0 0-.64 35.54c26.41 41.33 60.4 76.14 98.28 100.65C162 402 207.9 416 255.66 416c46.71 0 93.81-14.43 136.2-41.72c38.46-24.77 72.72-59.66 99.08-100.92a32.2 32.2 0 0 0-.1-34.76M256 352a96 96 0 1 1 96-96a96.11 96.11 0 0 1-96 96" />
+                        </svg>
+                    </div>
+
                 </div>
                 <button class="w-[500px] h-[45px] bg-[#A0E13E] text-[#ffffff] text-shadow-4xl rounded-[70px] font-bold shadow-2xl cursor-pointer hover:bg-[#80b432]"
                  style="text-shadow: 0px 0px 8px rgba(0,0,0,0.5);"
