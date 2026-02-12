eb78bd8f31d1550072104404afec421db9d3f14b
Chisanupong Srisaket <srisaket246@gmail.com>
2026-02-12 09:10:31 +0700

    fix

--- server/db.ts
+++ server/db.ts
@@ -5,9 +5,15 @@ if (!process.env.MYSQL_PUBLIC_URL) {
   throw new Error('❌ MYSQL_PUBLIC_URL is missing')
 }
 
-  export const db = mysql.createPool({
-    uri: process.env.MYSQL_PUBLIC_URL,
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true, // Aiven ต้องมี
+  },
   waitForConnections: true,
-  connectionLimit: 3,
-  queueLimit: 0,
+  connectionLimit: 5,
 })
